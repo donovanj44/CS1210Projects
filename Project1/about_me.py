aef6b2325e57ba6d3b2925a8ffcae0ed633587dd
Joe <jdonova9@uvm.edu>
2023-08-31 14:42:50 -0400

    Create about_me.py and HelloWorld.py

---
+++ Project1/about_me.py
@@ -0,0 +1,10 @@
+"""
+Joe Donovan
+jdonova9@uvm.edu
+CS1210
+"""
+print("My name is Joe Donovan")
+print("I am from Falls Church Virginia")
+print("I play hockey")
+print("I live in Central Campus Residence Hall")
+print("I have one dog")
